b7ba9ed3dc6f3a5fb3b231c9d70c6d0e3e927b01
Taisiia Strelnikova <tayushka1236@gmail.com>
2022-11-18 22:29:47 +0100

    function

--- js/script.js
+++ js/script.js
@@ -1,62 +1,85 @@
-//function printMessage(msg){
+/*function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
     printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
     document.getElementById('messages').appendChild(div);
-//}
-    //let computerMove='kamien',
-  //  let playerMove='papier',
-    //printMessage('Zagrałem ' computerMove + '! kamień! Jeśli Twój ruch to ' playerMove ', to wygrywasz!'),
-   // function clearMessages(){
-	//document.getElementById('messages').innerHTML = '';
-//}
+}
+    let computerMove='kamien',
+    let playerMove='papier',
+    printMessage('Zagrałem ' computerMove + '! kamień! Jeśli Twój ruch to ' playerMove ', to wygrywasz!'),
+    function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}*/
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }
+    if(argMoveId == 2){
+        return 'papier';
+      }
+      if(argMoveId == 3){
+        return 'nozyce';
+      } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+      }
+}
+function displayResult(argComputerMove, argPlayerMove){
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+        printMessage('Ty wygrywasz!');
+      } else if (argComputerMove == 'kamień' && argPlayerMove == 'nozyce')
+        {printMessage('Ja wygralem!');}
+    if( argComputerMove == 'papier' && argPlayerMove == 'nozyce'){
+        printMessage('Ty wygrywasz!');
+      } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień')
+      {printMessage('Ja wygralem!');}
+    if( argComputerMove == 'nozyce' && argPlayerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+      } else if (argComputerMove == 'nozyce' && argPlayerMove == 'papier')
+      {printMessage('Ja wygralem!');}
+    //bug
+    if( argPlayerMove == 'nieznany ruch')
+    {printMessage('input 1 or 2 or 3');}
+    
+    if( argComputerMove == argPlayerMove){
+        printMessage('Remis');} 
+}
+  
 //playerMove
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
+/*if(playerInput == '1'){
   playerMove = 'kamień';
 } else 
 if(playerInput == '2'){
     playerMove = 'papier';
 } else if(playerInput == '3') 
     {playerMove = 'nozyce';
-}
-printMessage('Twój ruch to: ' + playerMove);
+}*/
+//printMessage('Twój ruch to: ' + playerMove);
 //computerMove
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/*if(randomNumber == 1){
   computerMove = 'kamień';
 } else 
     if(randomNumber == 2){
     computerMove = 'papier';
   } else {computerMove = 'nozyce';
-}
-printMessage('Mój ruch to: ' + computerMove);
-
+}*/
+//printMessage('Mój ruch to: ' + computerMove);
+console.log('moves: ' + playerMove + ', ' + computerMove);
 //rezult
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  } else if (computerMove == 'kamień' && playerMove == 'nozyce')
-    {printMessage('Ja wygralem!');}
-if( computerMove == 'papier' && playerMove == 'nozyce'){
-    printMessage('Ty wygrywasz!');
-  } else if (computerMove == 'papier' && playerMove == 'kamień')
-  {printMessage('Ja wygralem!');}
-if( computerMove == 'nozyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-  } else if (computerMove == 'nozyce' && playerMove == 'papier')
-  {printMessage('Ja wygralem!');}
-//bug
-if( playerMove == 'nieznany ruch')
-{printMessage('input 1 or 2 or 3');}
-  
+
+displayResult(computerMove, playerMove);   
+
   
